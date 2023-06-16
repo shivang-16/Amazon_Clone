3a265151cdecdf47f4a87beadeac8a2f56aa1e3f
Shivang Yadav <shivangyadav121@gmail.com>
2023-06-16 22:15:58 +0530

    updated

--- script.js
+++ script.js
@@ -31,9 +31,43 @@ box3.addEventListener("click", function () {
   }
 });
 
+let box9 = document.querySelector('.nav-mini-mid-box9');
+let box9dropdown = document.querySelector('.nav-box9-dropdown-background');
+let box9dropdownbox = document.querySelector('.nav-box9-dropdown');
+
+box9.addEventListener('mouseenter', function(){
+  box9dropdown.style.display = "block";
+})
+box9.addEventListener('mouseleave', function(){
+  box9dropdown.style.display = "none";
+})
+box9dropdownbox.addEventListener('mouseenter', function(){
+  box9dropdown.style.display = "block";
+})
+box9dropdownbox.addEventListener('mouseleave', function(){
+  box9dropdown.style.display = "none";
+})
+
+let box6 = document.querySelector('.nav-box6');
+let box6dropdown = document.querySelector('.nav-box6-dropdown-background');
+let box6dropdownbox = document.querySelector('.nav-box6-dropdown');
+
+box6.addEventListener('mouseenter', function(){
+  box6dropdown.style.display = "block";
+})
+box6.addEventListener('mouseleave', function(){
+  box6dropdown.style.display = "none";
+})
+box6dropdownbox.addEventListener('mouseenter', function(){
+  box6dropdown.style.display = "block";
+})
+box6dropdownbox.addEventListener('mouseleave', function(){
+  box6dropdown.style.display = "none";
+})
 
 
-  let button1 = document.querySelector("#btn1");
+
+let button1 = document.querySelector("#btn1");
 let button2 = document.querySelector("#btn2");
 
 let img1 = document.querySelector('#img1');
@@ -45,5 +79,6 @@ button1.addEventListener('click', function(){
 button2.addEventListener('click', function(){
   img2.style.display = "block"
 })
+
 
 
